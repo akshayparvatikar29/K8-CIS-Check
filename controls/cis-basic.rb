de181d11d3f70f2d5b2cb2d5fb658ed5e6b534d8
akshayparvatikar29 <167433616+akshayparvatikar29@users.noreply.github.com>
2025-12-04 10:04:52 +0530

    Update cis-basic.rb

--- controls/cis-basic.rb
+++ controls/cis-basic.rb
@@ -10,7 +10,6 @@ control 'cis-1-1' do
   end
 end
 
-# This intentionally FAILS (we will fix it in step 3)
 control 'cis-1-2' do
   impact 0.7
   title 'Ensure /etc/shadow is properly permissioned'
@@ -18,13 +17,12 @@ control 'cis-1-2' do
 
   describe file('/etc/shadow') do
     it { should exist }
-    its('mode') { should cmp '0640' }   # This will FAIL because default is usually 640 or 600—but we force 640
+    its('mode') { should cmp '0640' }  
     its('owner') { should eq 'root' }
     its('group') { should eq 'shadow' }
   end
 end
 
-# This will PASS
 control 'cis-2-1' do
   impact 0.5
   title 'Ensure SSH Protocol 2 is enabled'
@@ -34,12 +32,11 @@ control 'cis-2-1' do
   end
 end
 
-# This intentionally FAILS (we fix in remediation)
 control 'cis-2-2' do
   impact 0.5
   title 'Ensure SSH root login is disabled'
 
   describe sshd_config do
-    its('PermitRootLogin') { should cmp 'no' } # Many cloud servers have it set to "prohibit-password", will FAIL
+    its('PermitRootLogin') { should cmp 'no' }
   end
 end
